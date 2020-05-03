290b9d9671380d3722f9b98e8c283a5db2b9c860
Atharva Lele <itsatharva@gmail.com>
2020-05-03 06:30:57 -0700

    m2: add Makefile
    Signed-off-by: Atharva Lele <itsatharva@gmail.com>

--- assessments/m2/src/sources.mk
+++ assessments/m2/src/sources.mk
@@ -10,8 +10,21 @@
 #*****************************************************************************
 
 # Add your Source files to this variable
-SOURCES =
+ifeq ($(PLATFORM), HOST)
+SOURCES = main.c \
+		  memory.c
+else
+ifeq ($(PLATFORM), MSP432)
+SOURCES = main.c \
+		  memory.c \
+		  interrupts_msp432p401r_gcc.c \
+		  startup_msp432p401r_gcc.c \
+		  system_msp432p401r.c
+endif
+endif
 
 # Add your include paths to this variable
-INCLUDES = 
+INCLUDES = -I../include/common \
+		   -I../include/CMSIS \
+		   -I../include/msp432
 
